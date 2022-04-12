cb4cf91bd4899fcc1c1b67b4b86c449db77f45a4
thomasbubi <4480324+thomasbubi@users.noreply.github.com>
2022-04-12 17:06:55 +0200

    connect with UDP Server

--- Utils/udp_server.js
+++ Utils/udp_server.js
@@ -1,28 +1,40 @@
 let data = {};
+let deviceNames = [];
 
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 
 server.on('message', function(msg, senderInfo){
-  console.log('server got message:' + msg);
+  //console.log('server got message:' + msg);
   let message = msg.toString();
     
   if(message.includes('get')) {
-      
-	  let sendMessage = '';
+      let m = message.split('_');
+      let deviceName = m[1];
+      let name = deviceName === deviceNames[0] ? deviceName[1] : deviceName[0];
+	  let sendMessage = '{"objects":[';//JSON.stringify(data);
+      for( const [key,value] of Object.entries(data[name]) ){
+          sendMessage += JSON.stringify(value) + ',';
+      }
+      sendMessage = sendMessage.substr(0, sendMessage.length - 1) + ']}';
 	  server.send(sendMessage,senderInfo.port,senderInfo.address);
   
   } else if(message.includes('set')){
       
       let m = message.split('_');
       let deviceName = m[1];
-      let retrievedObject = JSON.parse(m[2]);
+      let array = JSON.parse(m[2]);
       
       if(!data[deviceName]){
+          deviceNames.push(deviceName);
           data[deviceName] = {};
       }
       
-      data[deviceName][retrievedObject.name] = retrievedObject;
+      array.forEach( (obj) => {
+          data[deviceName][obj.name] = obj;
+      });
+      
+      console.log(data);
       
   }
   
@@ -32,6 +44,10 @@ server.on('listening', function(){
 	const address = server.address();
 	console.log('server is listening on ' + address.address + ':' + address.port);
 });
+
+setInterval( () => {
+    console.log(data)
+}, 1000) 
 
 server.bind({
 	address: 'localhost',
